f418a8b248d688d3850b4a60cf0e5018991c9d92
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-06-17 13:52:04 +1000

    Navbar updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,10 +21,9 @@ const NavBar = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("home")}>Home</Nav.Link>
-              <Nav.Link href="#timeline" className={activeLink === "timeline" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("timeline")}>My Journey</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === "projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
-              <Nav.Link href="#education" className={activeLink === "education" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("education")}>Education</Nav.Link>
+              <Nav.Link href="#timeline" className={activeLink === "timeline" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("timeline")}>My Journey</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
