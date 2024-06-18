6fcbe666aad99337b460e50d1ed8a5d136de998d
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-06-18 19:51:33 +1000

    Project card styling updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap"
 import projects from "../data/projects";
+import { ReactComponent as GlobeIcon } from "../assets/img/globe-icon.svg"
+import { ReactComponent as GithubIcon } from "../assets/img/github-icon.svg"
 
 const Projects = () => {
     return ( 
@@ -16,12 +18,20 @@ const Projects = () => {
                             <h3>{project.title}</h3>
                             <p>{project.description}</p>
                             <div className="project-links">
-                            <a href={project.liveDemo} className="btn btn-primary">Live Demo</a>
-                            <a href={project.sourceCode} className="btn btn-secondary">Source Code</a>
+                                <a href={project.liveDemo} className="project-button">
+                                    <div className="button-content-wrapper">
+                                        <GlobeIcon className="project-button-icon"/>Live Demo
+                                    </div>
+                                </a>
+                                <a href={project.sourceCode} className="project-button">
+                                    <div className="button-content-wrapper">
+                                        <GithubIcon  className="project-button-icon"/>Source Code
+                                    </div>
+                                </a>
                             </div>
                             <div className="project-tags">
                             {project.skills.map(skill => (
-                                <span key={skill} className="project-tag">{skill}</span>
+                                <div key={skill} className="project-tag">{skill}</div>
                             ))}
                             </div>
                         </div>
@@ -34,3 +44,4 @@ const Projects = () => {
 }
  
 export default Projects;
+
