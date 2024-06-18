7998ecbafc216f22ac000d03baf1434a59c63fc9
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-06-18 16:32:38 +1000

    Utility for exporting icons added. Timeline code revised

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,57 +1,13 @@
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
-
 import timelineElements from "../data/timelineElements"
-import { ReactComponent as WorkIcon } from "../assets/img/work.svg"
-import { ReactComponent as SchoolIcon } from "../assets/img/school-icon.svg"
-import { ReactComponent as StudyIcon } from "../assets/img/study-icon.svg"
-import { ReactComponent as UniIcon } from "../assets/img/uni-icon.svg"
-import { ReactComponent as TutorIcon } from "../assets/img/tutor-icon.svg"
-import { ReactComponent as FlightIcon } from "../assets/img/airplane-icon.svg"
-import { ReactComponent as CodingIcon } from "../assets/img/coding-icon.svg"
 import { Container } from "react-bootstrap"
-
-import { ReactComponent as GithubIcon } from '../assets/img/github-icon.svg';
+import { icons } from "../util/exportIcons"
 
 const Timeline = () => {
 
-    let workIconStyle = { background: "#121212" }; //#ff002e #06D6A0
-    let schoolIconStyle = { background: "#f9c74f" };
-
-    function renderWithStyling(text){
-        //let descriptionText = document.createElement( 'html' );
-        //descriptionText.innerHTML = text;
-        //console.log($(descriptionText))
-        //return descriptionText;
-        var temp = document.createElement('div');
-        temp.innerHTML = text;
-        return temp;
-    }
-
     function selectIcon(iconName){
-        let icon;
-        switch(iconName){
-            case "school":
-                icon = <SchoolIcon />
-                break;
-            case "study":
-                icon = <StudyIcon />
-                break;
-            case "university":
-                icon = <UniIcon />
-                break;
-            case "tutor":
-                icon = <TutorIcon />
-                break;
-            case "flight":
-                icon = <FlightIcon />
-                break;
-            case "coding":
-                icon = <CodingIcon />
-                break;
-        default:
-                break;
-        }
-        return icon;
+        let IconComponent = icons[iconName]; 
+        return <IconComponent />;
     }
 
     return ( 
---
+++ src/util/exportIcons.js
@@ -0,0 +1,15 @@
+import { ReactComponent as SchoolIcon } from "../assets/img/school-icon.svg"
+import { ReactComponent as StudyIcon } from "../assets/img/study-icon.svg"
+import { ReactComponent as UniIcon } from "../assets/img/uni-icon.svg"
+import { ReactComponent as TutorIcon } from "../assets/img/tutor-icon.svg"
+import { ReactComponent as FlightIcon } from "../assets/img/airplane-icon.svg"
+import { ReactComponent as CodingIcon } from "../assets/img/coding-icon.svg"
+
+export const icons = {
+    school: SchoolIcon,
+    study: StudyIcon,
+    university: UniIcon,
+    tutor: TutorIcon,
+    flight: FlightIcon,
+    coding: CodingIcon
+};
