c9d92c0873011ca274d87849f0954d74451856c1
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-06-18 18:54:41 +1000

    Project cards added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,33 @@
-import { Container, Row } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
+import projects from "../data/projects";
 
 const Projects = () => {
     return ( 
         <section className="projects" id="projects">
-            <Container>
+            <Container className="projects-container">
                 <Row>
                     <h1 className="section-title">Projects</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p className="section-description">I have used various technologies to develop applications. Here are some of my projects.</p>
+                </Row>
+                <Row className="projects-grid">
+                    {projects.map((project) => (
+                        
+                        <div key={project.id} className="project-card project-box">
+                            <img src={project.gif} alt={project.title} className="project-gif" />
+                            <h3>{project.title}</h3>
+                            <p>{project.description}</p>
+                            <div className="project-links">
+                            <a href={project.liveDemo} className="btn btn-primary">Live Demo</a>
+                            <a href={project.sourceCode} className="btn btn-secondary">Source Code</a>
+                            </div>
+                            <div className="project-tags">
+                            {project.skills.map(skill => (
+                                <span key={skill} className="project-tag">{skill}</span>
+                            ))}
+                            </div>
+                        </div>
+                   
+                    ))}
                 </Row>
             </Container>
         </section>
---
+++ src/data/projects.js
@@ -0,0 +1,122 @@
+const projects = [
+    {
+      id: 1,
+      title: "Project 1",
+      description: "Description for project one.",
+      gif: "path/to/project-one.gif",
+      liveDemo: "#",
+      sourceCode: "#",
+      skills: ["React", "CSS"]
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      description: "Description for project two.",
+      gif: "path/to/project-two.gif",
+      liveDemo: "#",
+      sourceCode: "#",
+      skills: ["Node", "React"]
+    },
+    {
+        id: 3,
+        title: "Project 3",
+        description: "Description for project one.",
+        gif: "path/to/project-one.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["React", "CSS"]
+      },
+      {
+        id: 4,
+        title: "Project 4",
+        description: "Description for project two.",
+        gif: "path/to/project-two.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["Node", "React"]
+      },
+      {
+        id: 5,
+        title: "Project 5",
+        description: "Description for project one.",
+        gif: "path/to/project-one.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["React", "CSS"]
+      },
+      {
+        id: 6,
+        title: "Project 6",
+        description: "Description for project two.",
+        gif: "path/to/project-two.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["Node", "React"]
+      },
+      {
+        id: 7,
+        title: "Project 7",
+        description: "Description for project one.",
+        gif: "path/to/project-one.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["React", "CSS"]
+      },
+      {
+        id: 8,
+        title: "Project 8",
+        description: "Description for project two.",
+        gif: "path/to/project-two.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["Node", "React"]
+      },
+      {
+        id: 9,
+        title: "Project 9",
+        description: "Description for project one.",
+        gif: "path/to/project-one.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["React", "CSS"]
+      },
+      {
+        id: 10,
+        title: "Project 10",
+        description: "Description for project two.",
+        gif: "path/to/project-two.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["Node", "React"]
+      },
+      {
+        id: 11,
+        title: "Project 11",
+        description: "Description for project one.",
+        gif: "path/to/project-one.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["React", "CSS"]
+      },
+      {
+        id: 12,
+        title: "Project 12",
+        description: "Description for project two.",
+        gif: "path/to/project-two.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["Node", "React"]
+      },
+      {
+        id: 13,
+        title: "Project 13",
+        description: "Description for project two.",
+        gif: "path/to/project-two.gif",
+        liveDemo: "#",
+        sourceCode: "#",
+        skills: ["Node", "React"]
+      }
+  ];
+  
+  export default projects;
+  
