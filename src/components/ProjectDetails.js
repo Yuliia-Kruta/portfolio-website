36458b372d4e0857489d0c0f7a870720daad2470
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-06-25 19:56:37 +1000

    Other components code refined

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,13 +1,12 @@
+import React from 'react';
 import { CloseRounded, GitHub, LinkedIn } from '@mui/icons-material';
 import { Modal } from '@mui/material';
-import React from 'react';
 import { ReactComponent as GlobeIcon } from "../assets/img/globe-icon.svg"
 import { ReactComponent as GithubIcon } from "../assets/img/github-icon.svg"
 
 const ProjectDetails = ({ openModal, setOpenModal }) => {
 
     const project = openModal?.project;
-    console.log(project);
 
     return (
         <Modal open={true} onClose={() => setOpenModal({ state: false, project: null })}>
