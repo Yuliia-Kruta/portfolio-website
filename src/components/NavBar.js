677c0149763f7b581a44e0c20fb8d7b606f24470
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-06-25 19:01:42 +1000

    Navbar component updated, active links highlight & scroll to sections fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar, Nav, Container} from "react-bootstrap"
+import { sectionIds } from "./SectionIds";
 
 const NavBar = () => {
     
     const [activeLink, setActiveLink] = useState("home");
-    
-    function onUpdateActiveLink(value){
-          setActiveLink(value);
+
+    function scrollToSection(sectionId){
+      const element = document.getElementById(sectionId);
+      if(element){
+        const marginTop = 0;
+        const scrollToY = element.getBoundingClientRect().top + window.scrollY - marginTop;
+        window.scrollTo({top: scrollToY, behavior: "smooth"});
+      }
     }
 
+    function determineActiveSection(){
+      for (let i = sectionIds.length-1; i>=0; i--){
+        const section = document.getElementById(sectionIds[i]);
+        if(section){
+          const rect = section.getBoundingClientRect();
+          if(rect.top <= 120 && rect.bottom >=120){
+            setActiveLink(sectionIds[i]);
+            break;
+          }
+        }
+      }
+    }
+
+    useEffect(() => {
+      const handleScroll = () => {
+        determineActiveSection();
+      }
+      window.addEventListener("scroll", handleScroll);
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      }
+    }, [])
+
     return ( 
-        <Navbar expand="md">
+      <Navbar expand="md">
         <Container>
           <Navbar.Brand href="/">
             Yuliia <span>Kruta</span>
@@ -20,10 +50,10 @@ const NavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("home")}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === "projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
-              <Nav.Link href="#timeline" className={activeLink === "timeline" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("timeline")}>My Journey</Nav.Link>
+              {sectionIds.map((sectionId, i) => (
+                <Nav.Link key={i} className={activeLink === sectionId ? "active navbar-link" : "navbar-link"} onClick={() => scrollToSection(sectionId)}>{
+                  sectionId==="timeline" ? "My Journey" : sectionId.charAt(0).toUpperCase() + sectionId.slice(1)}</Nav.Link>
+              ))}
             </Nav>
           </Navbar.Collapse>
         </Container>
